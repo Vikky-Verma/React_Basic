d8098b3b41c68e37c351ec6695b5c46a16f676cc
Vikky Verma <Vikkyverma7054@gmail.com>
2026-03-13 00:27:27 +0530

    project using npx create-react-app

---
+++ 00_reactProject.js
@@ -0,0 +1,27 @@
+// ways to create react project 
+// 1. using create-react-app
+// 2. using vite
+// 3. using webpack and babel
+
+// using create-react-app
+// 1. open terminal and run the command 
+// npx create-react-app my-app
+// 2. navigate to the project directory
+// cd my-app
+// 3. start the development server
+// npm start or npm run start
+// npm build  or npm run build to create a production build of the app it includes static files like html css and javascript and media files that can be deployed to a web server or hosting service.
+
+
+
+// using vite
+// 1. open terminal and run the command
+// npm create vite@latest my-app --template react
+// 2. navigate to the project directory
+// cd my-app
+// 3. install dependencies
+// npm install
+// 4. start the development server
+// npm run dev
+// npm build or npm run build to create a production build of the app it includes static files like html css and javascript and media files that can be deployed to a web server or hosting service.
+
