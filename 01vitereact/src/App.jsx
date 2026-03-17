5b0e3cb50eed4e1b51e47ec391f2f2931c4e7d94
Vikky Verma <Vikkyverma7054@gmail.com>
2026-03-18 01:31:33 +0530

    react library and jsx

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,10 +1,14 @@
 import Chai from "./chai";
 
 function App() { 
+  const username = "chai aur code"; 
+  // we can use javascript expression inside JSX by using curly braces {}. it will evaluate the expression and return the result. we can use any valid javascript expression inside curly braces {}. we can also use variables, functions, objects, arrays etc. inside curly braces. it will evaluate the expression and return the result.
+  // we cant use coditinal statements, loops , if else statements etc. inside JSX because they are not expressions, they are statements. we can use ternary operator instead of if else statements inside JSX because it is an expression. we can also use logical && operator to conditionally render elements inside JSX.
+
   return (
     <>  
       <Chai/>
-      <h1>Welcome to react </h1> 
+      <h1>Welcome to react {username}</h1> 
       <p>test paragraph</p>
     </>
   )
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,9 +1,56 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+// import {jsx as _jsx} from 'react/jsx-runtime' // we can use jsx without importing react because it is a new feature in react 17, but it is recommended to import react because it will work with state and lifecycle methods.
 import App from './App.jsx'
 
+
+function MyApp() {
+    return (
+        <div>
+            <h1>Custom App | chai</h1>
+        </div>
+    )
+}
+
+
+// const reactElement = {
+//     type :'a',
+//     props: {
+//         href : 'https://www.google.com',
+//         target : '_blank',
+//     },
+//     children: 'Click me to visit google'
+// }
+
+
+const anotherElement = (
+    <a href="https://www.google.com" target="_blank">
+        Visit google
+    </a>
+)
+
+const anotherUser = " chai aur react ";
+
+// React.createElement()  is a react library which is used to create a react element without using JSX, it takes three arguments, type, props and children. It returns a react element which is an object.
+
+const reactElement = React.createElement(
+    'a', // type
+    { href: 'https://www.google.com', target: '_blank' }, // props
+    'Click me to visit google', // children
+    anotherUser // we can also pass variables as children
+)
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  
-    <App />
+    // <App /> //
+    // <MyApp />
+    // MyApp() we can write like this also because it is function it is not recommended because it will not work with state and lifecycle methods.
+
+    // reactElement  // we can write like this also because it is a react element created using JSX, it is not recommended because it will work with state and lifecycle methods.
+
+    // anotherElement // we can write like this also because it is a react element created using JSX, it is recommended because it will work with state and lifecycle methods.
+
+    reactElement // we can write like this also because it is a react element created using React.createElement(), it is recommended because it will work with state and lifecycle methods. react library will convert this react element into a DOM element and render it on the screen.
+
+    // <App/>
   
 )
