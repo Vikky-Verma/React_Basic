0ecd5ed91189d8379081a0c7b16260a0c962cc85
Vikky Verma <Vikkyverma7054@gmail.com>
2026-03-24 17:13:51 +0530

    counter updation

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -25,13 +25,19 @@ function App() {
 
   const addValue = () => {
     if (counter < 20) {
-     setCounter(counter + 1);
+     setCounter(prevCounter => prevCounter+1); // setCounter recieves callback also to update the value
+     setCounter(prevCounter => prevCounter+1);
+     setCounter(prevCounter => prevCounter+1);
+     setCounter(prevCounter => prevCounter+1);
     }
   }
 
   const removeValue =() => {
     if (counter > 0) {
-      setCounter(counter - 1);
+      setCounter(prevCounter => prevCounter - 1);
+      setCounter(prevCounter => prevCounter - 1);
+      setCounter(prevCounter => prevCounter - 1);
+      setCounter(prevCounter => prevCounter - 1);
     }
   }
 
